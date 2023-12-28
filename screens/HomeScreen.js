b323ea8ac0d6c9533413f37817c1d9561aaf9c55
yudhistira arya <aryarajasa0@gmail.com>
2023-12-28 10:30:06 +0800

    router

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 relative">
+      <Text>sssssss</Text>
+    </View>
+  );
+}
